4cd232ab01e63d3909acf05cfbe35103a9628170
Vojtech Jasny <voy@miro.com>
2026-04-05 22:56:18 +0200

    Add demo tabs script

---
+++ scripts/demo-tabs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" \
+  --profile-directory="Profile 1" \
+  --new-window \
+  "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array" \
+  "https://github.com/microsoft/vscode" \
+  "https://github.com/microsoft/vscode/issues" \
+  "https://github.com/facebook/react" \
+  "https://github.com/facebook/react/issues" \
+  "https://github.com/sindresorhus/awesome" \
+  "https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array" \
+  "https://stackoverflow.com/questions/1145777/how-to-convert-a-query-string-into-a-javascript-object" \
+  "https://stackoverflow.com/questions/38616612/javascript-elegant-way-to-check-object-has-required-properties" \
+  "https://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-and-arrays-by-string-path" \
+  "https://news.ycombinator.com/" \
+  "https://news.ycombinator.com/item?id=38932891" \
+  "https://news.ycombinator.com/item?id=39062709" \
+  2>/dev/null &
